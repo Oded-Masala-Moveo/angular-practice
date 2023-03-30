560dc99942740e2002a80395c8fab092f6fdb539
Oded Masala <odedm@moveo.co.il>
2023-03-30 14:23:53 +0300

    update'

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -12,7 +12,7 @@ import { AccountsService } from '../accounts.service';
 export class NewAccountComponent {
 
   constructor(private loggingService: LoggingService,
-              private accountsService: AccountsService) {
+    private accountsService: AccountsService) {
     this.accountsService.statusUpdated.subscribe(
       (status: string) => alert('New Status: ' + status)
     );
